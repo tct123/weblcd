578238a7d1fdab89104a5ba6c4b3d613947c2817
tct123 <42028373+tct123@users.noreply.github.com>
2023-10-10 19:49:53 +0200

    .

--- src/weblcd/app.py
+++ src/weblcd/app.py
@@ -1,6 +1,23 @@
+from nicegui import *
+import gpiozero as gz
+
 """
 My first application
 """
+
+
+class WebLCD:
+    def __init__(self, pin):
+        self.pin = pin
+
+    def gui(self):
+        @ui.page("/")
+        def home():
+            textfield = ui.input(placeholder="input")
+
+    ui.run()
+
+
 def main():
-    # This should start and launch your app!
-    pass
+    myapp = WebLCD(pin=1)
+    myapp.gui()
